4278146d7a17d3cb454feadde85b46a60e8d2d09
Alex Omar <omat112694@gmail.com>
2017-03-02 01:30:37 -0800

    Adding networking code to connect to ground server

---
+++ Network.h
@@ -0,0 +1,30 @@
+//Network.h
+//
+
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+using namespace std;
+
+class Network
+{
+    public:
+        Network();
+        int nwrite(string message);
+        string nread()
+        void connect_ground_server();
+
+    private:
+        struct sockaddr_in ground_server;
+        int ground_socket;
+
+
+
+};
+
+#endif
